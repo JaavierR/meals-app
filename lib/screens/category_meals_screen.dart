cefe1b92571a86b59046ff4a9b9e325c44e52318
Javier Reyes <javier.reys@gmail.com>
2020-09-16 20:07:01 -0300

    Added cards to display meals in the meal screen

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   // Common practice is to add static constant property to my screens
   static const routeName = '/category-meals';
@@ -18,13 +21,25 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('The recipes for that category!'),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
